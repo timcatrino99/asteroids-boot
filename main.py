6c393062b8ef299fa337803f0af474951ea07cca
timcatrino99 <timcatrino99@gmail.com>
2025-08-08 17:37:53 -0400

    Player draws on screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -7,13 +8,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps_clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        pygame.Surface.fill(screen, (0,0,0))
+        pygame.Surface.fill(screen, "black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = pygame.time.Clock.tick(fps_clock, 60)/1000
 
 
 if __name__ == "__main__":
