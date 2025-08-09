c9dc2ed9dc5566d5917d8a5eea3c08942f4fe8a0
timcatrino99 <timcatrino99@gmail.com>
2025-08-09 14:58:17 -0400

    Player movement functional

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     fps_clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -18,7 +22,9 @@ def main():
                 return
             
         pygame.Surface.fill(screen, "black")
-        player.draw(screen)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         dt = pygame.time.Clock.tick(fps_clock, 60)/1000
 
